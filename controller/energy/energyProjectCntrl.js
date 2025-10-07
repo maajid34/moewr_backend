46858632a7799abb2f49539cd84d4e8b7b632cb7
abdi majiid haji <najacade23456@gmail.com>
2025-10-07 14:43:37 +0300

    chaneged

--- controller/energy/energyProjectCntrl.js
+++ controller/energy/energyProjectCntrl.js
@@ -219,8 +219,8 @@ const readProjectEnergy = async (req, res) => {
 
     // Sort manually: incomplete first, completed last
     const sortedProjects = projects.sort((a, b) => {
-      const aCompleted = a.projectStage?.toLowerCase() === "completed";
-      const bCompleted = b.projectStage?.toLowerCase() === "completed";
+      const aCompleted = a.projectSatge?.toLowerCase() === "completed";
+      const bCompleted = b.projectSatge?.toLowerCase() === "completed";
 
       if (aCompleted && !bCompleted) return 1;  // Completed goes down
       if (!aCompleted && bCompleted) return -1; // Not completed goes up
